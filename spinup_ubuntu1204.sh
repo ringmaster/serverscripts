1692ebdc6746604768ea9a8eda41b65b578605f2
ringmaster <epithet@gmail.com>
2013-06-03 15:41:51 +0000

    Add node, better package management

--- spinup_ubuntu1204.sh
+++ spinup_ubuntu1204.sh
@@ -12,7 +12,7 @@
 # PEAR is installed and APC is enabled
 # memcached is installed
 
-INSTALL_LOG=/var/log/spinup_ubuntu1010.log
+INSTALL_LOG=/var/log/spinup_ubuntu1204.log
 
 # Make sure we didn't already run this script. Probably wouldn't /really/ hurt,
 # but just to be careful...
@@ -26,7 +26,6 @@ touch $INSTALL_LOG
 function installnoninteractive() {
 	local packages="$1"
 	echo "Installing: $packages"
-	echo "(don't worry if there isn't much output, it's being logged here: $INSTALL_LOG)"
 	sudo bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -q -y $packages >> $INSTALL_LOG"
 }
 
@@ -36,21 +35,34 @@ if [ `id -u` != 0 ]; then
 	exit 1
 fi
 
-# Add multiverse to the repository sources file.
-cat >> /etc/apt/sources.list <<APT_SOURCES
+echo "Don't worry if there isn't much output, it's being logged here: $INSTALL_LOG"
 
-# Multiverse repositories. !!Required for some server packages!! DO NOT REMOVE!!
-deb http://us.archive.ubuntu.com/ubuntu/ precise multiverse
-deb-src http://us.archive.ubuntu.com/ubuntu/ precise multiverse
-deb http://us.archive.ubuntu.com/ubuntu/ precise-updates multiverse
-deb-src http://us.archive.ubuntu.com/ubuntu/ precise-updates multiverse
-APT_SOURCES
+installnoninteractive "python-software-properties python g++ make"
+
+add-apt-repository -y 'deb http://us.archive.ubuntu.com/ubuntu/ precise multiverse'
+add-apt-repository -y 'deb-src http://us.archive.ubuntu.com/ubuntu/ precise multiverse'
+add-apt-repository -y 'deb http://us.archive.ubuntu.com/ubuntu/ precise-updates multiverse'
+add-apt-repository -y 'deb-src http://us.archive.ubuntu.com/ubuntu/ precise-updates multiverse'
 
 # Get new list of packages.
 echo "Updating package lists..."
 apt-get -q -y update >> $INSTALL_LOG
 
-installnoninteractive "python-software-properties openssh-server mysql-server nginx php5-cgi php5-fpm php-pear php5-dev sqlite3 memcached curl php5-gd php5-mcrypt php5-memcache php5-mhash php5-curl php5-imap php5-ldap php5-mysql php5-sqlite php5-pspell php5-tidy php-apc postfix git-core lrzsz zsh tmux vim python2.7-doc binutils binfmt-support ctags vim-doc vim-scripts indent"
+# Add multiverse to the repository sources file.
+#cat >> /etc/apt/sources.list <<APT_SOURCES
+
+# Multiverse repositories. !!Required for some server packages!! DO NOT REMOVE!!
+# deb http://us.archive.ubuntu.com/ubuntu/ precise multiverse
+# deb-src http://us.archive.ubuntu.com/ubuntu/ precise multiverse
+# deb http://us.archive.ubuntu.com/ubuntu/ precise-updates multiverse
+# deb-src http://us.archive.ubuntu.com/ubuntu/ precise-updates multiverse
+# APT_SOURCES
+
+installnoninteractive "openssh-server mysql-server nginx php5-cgi php5-fpm php-pear php5-dev sqlite3 memcached curl php5-gd php5-mcrypt php5-memcache php5-mhash php5-curl php5-imap php5-ldap php5-mysql php5-sqlite php5-pspell php5-tidy php-apc postfix git-core lrzsz zsh tmux vim python2.7-doc binutils binfmt-support ctags vim-doc vim-scripts indent"
+
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get update
+installnoninteractive "nodejs"
 
 adduser --system --no-create-home nginx
 
@@ -109,32 +121,66 @@ mkdir -p /etc/nginx/conf.d
 mkdir -p /etc/nginx/sites-available
 mkdir -p /etc/nginx/sites-enabled
 
-cat > /etc/nginx/conf.d/fastcgi_param.conf <<FCGI_PARAMS
-fastcgi_param	QUERY_STRING		$query_string;
-fastcgi_param	REQUEST_METHOD		$request_method;
-fastcgi_param	CONTENT_TYPE		$content_type;
-fastcgi_param	CONTENT_LENGTH		$content_length;
+cat > /etc/nginx/fastcgi_params <<FCGI_PARAMS
+fastcgi_param	QUERY_STRING		\$query_string;
+fastcgi_param	REQUEST_METHOD		\$request_method;
+fastcgi_param	CONTENT_TYPE		\$content_type;
+fastcgi_param	CONTENT_LENGTH		\$content_length;
 
-fastcgi_param	SCRIPT_NAME		$fastcgi_script_name;
-fastcgi_param	REQUEST_URI		$request_uri;
-fastcgi_param	DOCUMENT_URI		$document_uri;
-fastcgi_param	DOCUMENT_ROOT		$document_root;
-fastcgi_param	SERVER_PROTOCOL		$server_protocol;
-fastcgi_param   SCRIPT_FILENAME 	$document_root$fastcgi_script_name;
-fastcgi_param   PATH_INFO 		$fastcgi_script_name;
+fastcgi_param	SCRIPT_NAME		\$fastcgi_script_name;
+fastcgi_param	REQUEST_URI		\$request_uri;
+fastcgi_param	DOCUMENT_URI		\$document_uri;
+fastcgi_param	DOCUMENT_ROOT		\$document_root;
+fastcgi_param	SERVER_PROTOCOL		\$server_protocol;
+fastcgi_param   SCRIPT_FILENAME 	\$document_root$fastcgi_script_name;
+fastcgi_param   PATH_INFO 		\$fastcgi_script_name;
 
 fastcgi_param	GATEWAY_INTERFACE	CGI/1.1;
-fastcgi_param	SERVER_SOFTWARE		nginx/$nginx_version;
+fastcgi_param	SERVER_SOFTWARE		nginx/\$nginx_version;
 
-fastcgi_param	REMOTE_ADDR		$remote_addr;
-fastcgi_param	REMOTE_PORT		$remote_port;
-fastcgi_param	SERVER_ADDR		$server_addr;
-fastcgi_param	SERVER_PORT		$server_port;
-fastcgi_param	SERVER_NAME		$server_name;
+fastcgi_param	REMOTE_ADDR		\$remote_addr;
+fastcgi_param	REMOTE_PORT		\$remote_port;
+fastcgi_param	SERVER_ADDR		\$server_addr;
+fastcgi_param	SERVER_PORT		\$server_port;
+fastcgi_param	SERVER_NAME		\$server_name;
 
 # PHP only, required if PHP was built with --enable-force-cgi-redirect
 fastcgi_param	REDIRECT_STATUS		200;
 FCGI_PARAMS
+
+cat >> /etc/nginx/sites-available/VDR <<VDR
+server {
+    server_name  ~^www\.(?P<wwwdomain>.*)$;
+    rewrite ^(.*) http://$wwwdomain$1 permanent;
+}
+
+server {
+        listen 80 default_server;
+        server_name ~^(?P<domain>.+)$;
+        root   /var/www/$domain/htdocs;
+        index index.php;
+	# include /etc/nginx/security;
+
+	# Logging --
+	access_log  /var/log/nginx/$domain.access.log;
+	error_log  /var/log/nginx/$domain.error.log notice;
+
+        # serve static files directly
+        location ~* ^.+.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt)$ {
+            access_log        off;
+            expires           max;
+        }
+ 
+        location ~ \.php$ {
+		try_files $uri =404;
+                fastcgi_pass unix:/var/run/php5-fpm/$domain.socket;
+                fastcgi_index index.php;
+                include /etc/nginx/fastcgi_params;
+        }
+}
+VDR
+
+ln -s /etc/nginx/sites-available/VDR /etc/nginx/sites-enabled/VDR
 
 cat >> /etc/php5/conf.d/apc.ini <<APC_INI
 apc.shm_size = 48
@@ -142,7 +188,5 @@ apc.include_once_override = 1
 apc.mmap_file_mask = /tmp/apc.XXXXXX
 APC_INI
  
-/etc/init.d/php-fpm start
+/etc/init.d/php5-fpm start
 /etc/init.d/nginx start
-
-
